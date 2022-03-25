e0399c9216370bece3e1eaf89e3875c74594b5ce
figmulberry <mosestkamau@gmail.com>
2022-03-25 12:07:21 -0700

    A C program that prints the size of various types on the computer it is compiled and run on. The Program returns a 0

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - Entry point
+*Return: Always 0 when program runs
+*/
+int main(void)
+{
+int intType;
+float floatType;
+double doubleType;
+char charType;
+printf("Size of int: %zu bytes\n", sizeof(intType));
+printf("Size of float: %zu bytes\n", sizeof(floatType));
+printf("Size of double: %zu bytes\n", sizeof(doubleType));
+print("Size of char: %zu byte\n", sizeof(charType));
+return (0);
+}
